9983890ce1180eabcea9328a778156c9de4fc231
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2022-12-07 14:31:48 +0700

    yuhu

--- Introduction/src/introduction/Introduction.java
+++ Introduction/src/introduction/Introduction.java
@@ -1,16 +1,38 @@
 package introduction;
 
-/**
- *
- * @author bcamaster
- */
-public class Introduction {
+import java.util.Iterator;
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
+public class Introduction 
+{
+    Scanner sc = new Scanner(System.in);
+    public Introduction()
+    {
+        int input;
+        String name;
+
+        System.out.print("Input some value: ");
+        input = sc.nextInt();
+        sc.nextLine();
+
+        System.out.printf("Input your name: ");
+        name = sc.nextLine();
+
+        System.out.println("n = " + input + ", Your name is " + name);
+    }
+    public static void main(String[] args)
+    {
+        new Introduction();
+        int n = 10;
+        String input;
+
+        if(n > 5)
+        {
+            System.out.println("Angka = " + n + " Hebat");
+        }
+
+        System.out.println("Hello World");
+        System.out.println("Good Evening!");
     }
     
 }
