5802072221fb0bdc9011fc1b95a073ce98ab73b6
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2023-03-15 14:33:44 +0700

    Session 09

--- JavaGUI/src/main/java/com/example/javagui/JavaForm.java
+++ JavaGUI/src/main/java/com/example/javagui/JavaForm.java
@@ -1,16 +1,59 @@
 package com.example.javagui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class JavaForm extends JFrame
+public class JavaForm extends JFrame implements ActionListener
 {
+    private JMenuBar menuBar = new JMenuBar();
+    private JMenu menuFile = new JMenu("File");
+    private JMenuItem menuItemFileLogin = new JMenuItem("Login");
+    private JMenuItem menuItemFile = new JMenuItem("Form 1");
+    private JMenu menuOption = new JMenu("Option");
+    private LoginForm loginForm;
+    private JDesktopPane desktopPane = new JDesktopPane();
+
         public JavaForm()
         {
+            add(desktopPane);
+            menuBar.add(menuFile);
+            menuFile.add(menuItemFileLogin);
+            menuFile.add(new JSeparator());
+            menuFile.add(menuItemFile);
+
+            menuBar.add(menuOption);
+            menuOption.setVisible(false);
+
+            setJMenuBar(menuBar);
+            menuItemFileLogin.addActionListener(this);
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
             setExtendedState(MAXIMIZED_BOTH);
             setVisible(true);
         }
         public static void main (String[] args)
         {
-
+            new JavaForm();
         }
+
+        public void doLogin()
+        {
+            menuOption.setVisible(true);
+        }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getSource().equals(menuItemFileLogin))
+        {
+//            JOptionPane.showMessageDialog(null, "Login");
+//            desktopPane.add(loginForm);
+//            loginForm.setVisible(true);
+            if(loginForm == null || loginForm.isClosed())
+            {
+                loginForm = new LoginForm(this);
+                desktopPane.add(loginForm);
+            }
+        }
+    }
 }
---
+++ JavaGUI/src/main/java/com/example/javagui/LoginForm.java
@@ -0,0 +1,30 @@
+package com.example.javagui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginForm extends JInternalFrame implements ActionListener
+{
+    private JButton btnLogin = new JButton("Login");
+    private JavaForm javaForm = new JavaForm();
+    public LoginForm(JavaForm javaForm)
+    {
+        this.javaForm = javaForm;
+
+        setLayout(new FlowLayout());
+        add(btnLogin);
+
+        setClosable(true);
+        setSize(300, 400);
+        setTitle("Login Form");
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+
+    }
+}
